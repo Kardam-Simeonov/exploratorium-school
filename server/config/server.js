c8306477f442c2c68a4e0bd00ea420c007001bf2
Kardam-Simeonov <masutayodagm@gmail.com>
2025-03-19 13:52:54 +0200

    removed url

--- server/config/server.js
+++ server/config/server.js
@@ -3,7 +3,6 @@ const crypto = require('crypto');
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://127.0.0.1:1337'),
   app: {
     keys: env.array('APP_KEYS', [crypto.randomBytes(16).toString('hex')]),
   },
