9333eaaa3d6b060f0a64336aefb181a50b9b8bca
Kardam-Simeonov <masutayodagm@gmail.com>
2025-03-19 16:29:12 +0200

    public url

--- server/config/server.js
+++ server/config/server.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('PUBLIC_URL', 'https://127.0.0.1'),
   app: {
     keys: env.array('APP_KEYS', [crypto.randomBytes(16).toString('hex')]),
   },
