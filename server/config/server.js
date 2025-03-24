b5a0fdf27b00964e8e511e609c1804f74d24507c
Kardam-Simeonov <masutayodagm@gmail.com>
2025-03-24 13:04:49 +0200

    set url to env var

--- server/config/server.js
+++ server/config/server.js
@@ -3,6 +3,7 @@ const crypto = require('crypto');
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('SERVER_URL'),
   app: {
     keys: env.array('APP_KEYS', [crypto.randomBytes(16).toString('hex')]),
   },
